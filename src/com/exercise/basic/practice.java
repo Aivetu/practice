49281952f7ef268c2bbfb5136a93261f0829ae98
Aivetu <harunahabibat2@gmail.com>
2021-10-03 19:39:56 +0100

    sort integer and string array

--- src/com/exercise/basic/practice.java
+++ src/com/exercise/basic/practice.java
@@ -1,4 +1,24 @@
 package com.exercise.basic;
 
+import java.util.Arrays;
+
 public class practice {
+    public static void main(String[] args) {
+        int[] numbers= {3,5,2,5,4,3,2,5,4,3,2};
+        Arrays.sort(numbers);
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+        String[] names= {"ojo","temi","titi","olorunfemi","amaka","annabelle"};
+        Arrays.sort(names);
+
+        for (String name : names) {
+            System.out.println(name);
+
+        }}
+
+
+
+
 }
