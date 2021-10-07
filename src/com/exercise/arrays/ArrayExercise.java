4a19d33a65b86ea350bb1ea664431880833b056a
abdulgafar.obeitor <aobeitor@yahoo.com>
2021-10-07 13:59:03 +0100

    question added

--- src/com/exercise/arrays/ArrayExercise.java
+++ src/com/exercise/arrays/ArrayExercise.java
@@ -2,4 +2,49 @@ package com.exercise.arrays;
 
 public class ArrayExercise {
 
+    /**
+     * complete all other methods, do not change anything in the main method, run your code
+     * @param args
+     */
+    public static void main(String[] args) {
+        intArray = initializeArray();
+        displayArray();// display - 3,6,2,4,8
+        replace3rdItem(5);
+        displayArray();//display - 3,6,5,4,8
+        multiplyBy(2);
+        displayArray();//display - 6,12,10,8,16
+    }
+
+
+    private static int[] intArray;
+
+    /**
+     * Initialize the array with 3,6,2,4,8
+     * @return
+     */
+    private static int[] initializeArray(){
+        //replace code below to initialize array here with value and return it
+        return null;
+    }
+
+    /**
+     * Code should display the numbers in the array seperated with comma e.g 3,6,2,4,8
+     */
+    private static void displayArray(){
+        //enter code here
+    }
+
+    /**
+     * Enter code to replace the item 3 with whatever is in the method argument
+     */
+    private static void replace3rdItem(int newValue){
+        //enter code here
+    }
+
+    /**
+     * Enter code to multiply all items in the array by the argument
+     */
+    private static void multiplyBy(int multiplier){
+        //enter code here
+    }
 }
