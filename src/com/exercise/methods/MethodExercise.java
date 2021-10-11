b325d5607113fad0271d538ab15756b0a6d8a31c
abdulgafar.obeitor <aobeitor@yahoo.com>
2021-10-11 10:04:16 +0100

    method additional exercise

--- src/com/exercise/methods/MethodExercise.java
+++ src/com/exercise/methods/MethodExercise.java
@@ -10,4 +10,11 @@ public class MethodExercise {
      * int summation(int[])
      */
 
+
+    /**
+     * 1. Create method max to take two numbers as argument and return the bigger one
+     * 2. Create method min to take an array of number and return the smallest
+     * 3. Create method to take an array of numbers and return the average
+     * 4. Create a method that takes an integer and return the number of digits e.g 2345 returns 4
+     */
 }
