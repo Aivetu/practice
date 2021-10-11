a29c4ae120bbfb1f3254b772b03c007144fec8b2
abdulgafar.obeitor <aobeitor@yahoo.com>
2021-10-11 08:13:25 +0100

    method sample exercise

---
+++ src/com/exercise/methods/MethodExercise.java
@@ -0,0 +1,13 @@
+package com.exercise.methods;
+
+public class MethodExercise {
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * int summation(int[])
+     */
+
+}
