de6ae2dc0f66e6e2f76f142c41eecb271e886a13
Aivetu <harunahabibat2@gmail.com>
2021-10-11 10:01:43 +0100

    methods exercise

--- src/com/exercise/methods/MethodExercise.java
+++ src/com/exercise/methods/MethodExercise.java
@@ -3,11 +3,20 @@ package com.exercise.methods;
 public class MethodExercise {
 
     public static void main(String[] args) {
-        
+       int[] intArray={2,5,2,8,6,5,6};
+       System.out.println(summation(intArray));
     }
 
     /**
      * int summation(int[])
      */
+
+    public static int  summation(int[] args) {
+        int sum=0;
+        for( int i : args){
+            sum += i;
+        }
+        return sum;
+    }
 
 }
