b0bc7e434fdaa6f8643501c2da40dcca2810a273
Aivetu <harunahabibat2@gmail.com>
2021-10-11 11:39:20 +0100

    answers to methods exercise

--- src/com/exercise/methods/MethodExercise.java
+++ src/com/exercise/methods/MethodExercise.java
@@ -1,10 +1,16 @@
 package com.exercise.methods;
 
+import java.util.Arrays;
+
 public class MethodExercise {
 
     public static void main(String[] args) {
        int[] intArray={2,5,2,8,6,5,6};
        System.out.println(summation(intArray));
+       System.out.println(biggerOne(5,7));
+       System.out.println(smallestArray(intArray));
+       System.out.println(averageNum(intArray));
+       System.out.println(numDigits(32736));
     }
 
     /**
@@ -17,6 +23,36 @@ public class MethodExercise {
             sum += i;
         }
         return sum;
+    }
+    public static int biggerOne(int item,int no){
+        if (item> no){
+            return item;
+        }
+        else return no;
+    }
+    public static int smallestArray(int[] args){
+        Arrays.sort(args);
+        for(int x:args){
+            return args[0];
+
+        }
+        return args[0];
+    }
+    public static int averageNum(int[] args){
+        int total=0;
+        for (int y:args){
+            total += y;
+        }
+        int mean =total / args.length;
+        return mean;
+    }
+    public static int numDigits(int digits){
+        int count=0;
+        while(digits!=0){
+            digits=digits/10;
+            count++;
+        }
+        return count;
     }
 
 }
