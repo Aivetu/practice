0b2322e5f5da612225ddf1fc0f0f19538ef53b53
Aivetu <harunahabibat2@gmail.com>
2021-10-13 13:27:59 +0100

    answers to class exercise

--- src/com/exercise/classes/MainClass.java
+++ src/com/exercise/classes/MainClass.java
@@ -6,5 +6,9 @@ public class MainClass {
         //Create a new User with username and password
         //call the sign in method and print its response when the right credentials are sent
         // call the sign in method and print its response when the wrong credentials are sent
+    User print = new User();
+    System.out.println(print.confirmPassword("aiva","jdhfhd"));
+    System.out.println(print.confirmPassword("Aivetu","ilikefood"));
+
     }
 }
--- src/com/exercise/classes/User.java
+++ src/com/exercise/classes/User.java
@@ -1,12 +1,30 @@
 package com.exercise.classes;
 
+import java.util.Scanner;
+
 public class User {
     private String username;
     private String password;
+
 
     /**
      * 1. Create constructor that has the two fields as properties
      * 2. Create method to sign in that will accept two arguments: username and password
      * and returns boolean true if username and password matches and false if it does not
      */
+
+    public User() {
+        username = "Aivetu";
+        password = "ilikefood";
+    }
+
+    public boolean confirmPassword(String uname,String pword) {
+        if ( uname==username && pword == password){
+            return true;
+        }
+        else return false;
+
+    }
 }
+
+
